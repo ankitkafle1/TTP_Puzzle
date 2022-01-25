d93ad6f1d4644e77d8a66c4a334e27b4aa1b4f87
Ankit69 <51173448+Ankit69@users.noreply.github.com>
2022-01-25 13:50:37 -0500

    reverse.js completed

--- reverse.js
+++ reverse.js
@@ -2,13 +2,27 @@
 //return the new string. Every word should be reversed but the string as a whole should not 
 //be reversed. And DO NOT use array.reverse() method
 //function to reverse the string 
-function stringReverse(str){
-    newStr =""
-    for(let i = str.length-1; i >=0; i--){
-        newStr += str[i]
+
+// a function which reverses the string
+function stringRev(st){
+    let newStr =""
+    for(let i = (st.length - 1); i >=0; i--){
+        newStr += st[i]
     }
     return newStr
 }
 
-a =stringReverse("ankit kafle")
-console.log(a)
+//a function wich first seperate string into array, dilimiter " ", and then pass array element to stringRev function. to reverse string per word.
+function stringReverse(str){
+    let tempArr = str.split(" ")
+    console.log(tempArr.length)
+    let newStr =""
+
+    for(let j = 0; j < tempArr.length ; j++){
+        newStr = newStr + stringRev(tempArr[j])+ " "
+    }
+    return newStr
+}
+
+b = stringReverse("I am happ'y I want to go to pokhara. ")
+console.log(b)
